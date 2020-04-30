65f3a0ec788803f55f63bdcaffd622f859ad054f
Zef Delgadillo <zef.delgadillo@gmail.com>
2020-04-30 13:20:09 -0400

    start gce function

---
+++ functions/launch-compute-engine/main.js
@@ -0,0 +1,64 @@
+const Compute = require('@google-cloud/compute');
+const compute = new Compute();
+
+/**
+ * Starts Compute Engine instances.
+ *
+ * Expects a PubSub message with JSON-formatted event data containing the
+ * following attributes:
+ *  zone - the GCP zone the instances are located in.
+ *  label - the label of instances to start.
+ *
+ * @param {!object} event Cloud Function PubSub message event.
+ * @param {!object} callback Cloud Function PubSub callback indicating
+ *  completion.
+ */
+exports.startInstancePubSub = async (event, context, callback) => {
+  try {
+    const options = {filter: `labels.${process.env.label}`};
+    const [vms] = await compute.getVMs(options);
+    await Promise.all(
+      vms.map(async (instance) => {
+        if (payload.zone === instance.zone.id) {
+          const [metadata] = await compute 
+            .zone(payload.zone)
+            .vm(instance.name)
+            .setMetadata({
+              'event': event,
+              'context': context
+            });
+          const [operation] = await compute
+            .zone(payload.zone)
+            .vm(instance.name)
+            .start();
+
+          // Operation pending
+          return [metadata.promise(), operation.promise()];
+        }
+      })
+    );
+
+    // Operation complete. Instance successfully started.
+    const message = `Successfully started instance(s)`;
+    console.log(message);
+    callback(null, message);
+  } catch (err) {
+    console.log(err);
+    callback(err);
+  }
+};
+
+/**
+ * Validates that a request payload contains the expected fields.
+ *
+ * @param {!object} payload the request payload to validate.
+ * @return {!object} the payload object.
+ */
+const _validatePayload = (payload) => {
+  if (!payload.zone) {
+    throw new Error(`Attribute 'zone' missing from payload`);
+  } else if (!payload.label) {
+    throw new Error(`Attribute 'label' missing from payload`);
+  }
+  return payload;
+};
